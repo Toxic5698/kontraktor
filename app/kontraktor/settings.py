3a834d81a69d2cd29d79c2f7fdb3b521e9cf8347
petr <petr.cech@netbox.cz>
2023-02-03 20:20:16 +0100

    docker final?

--- app/kontraktor/settings.py
+++ app/kontraktor/settings.py
@@ -134,7 +134,7 @@ USE_THOUSAND_SEPARATOR = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'staticfiles/'),
 #
