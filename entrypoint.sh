7a014a4044e97b24bc52aa1047fb98453fa3c1b9
petr <petr.cech@netbox.cz>
2023-02-04 12:01:04 +0100

    docker features 3

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ python manage.py collectstatic --no-input
 
 export DJANGO_SUPERUSER_EMAIL=petr@cechpetr.cz
 export DJANGO_SUPERUSER_USERNAME=petr
-export DJANGO_SUPERUSER_PASSWORD=12345
+export DJANGO_SUPERUSER_PASSWORD=a2345
 python manage.py createsuperuser --noinput --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
 
 gunicorn kontraktor.wsgi:application --bind 0.0.0.0:8000 --workers 2
