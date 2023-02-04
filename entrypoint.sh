a6a77b3d9d7a9e6e0689e9447149f38c8c543aff
petr <petr.cech@netbox.cz>
2023-02-04 12:06:15 +0100

    password obstrukce

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ python manage.py collectstatic --no-input
 
 export DJANGO_SUPERUSER_EMAIL=petr@cechpetr.cz
 export DJANGO_SUPERUSER_USERNAME=petr
-export DJANGO_SUPERUSER_PASSWORD=a2345
-python manage.py createsuperuser --noinput --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
-
+export DJANGO_SUPERUSER_PASSWORD=12345
+DJANGO_SUPERUSER_PASSWORD=12345 python manage.py createsuperuser --no-input --username=petr  --email=petr@cechpetr.cz
 gunicorn kontraktor.wsgi:application --bind 0.0.0.0:8000 --workers 2
