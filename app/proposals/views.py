b821a1e98654ea1727d6f19fcbc2285e7d3469d1
petr <petr.cech@netbox.cz>
2023-06-19 13:54:45 +0200

    feat: added handover protocol part 3

--- app/proposals/views.py
+++ app/proposals/views.py
@@ -1,6 +1,5 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.template.response import TemplateResponse
 from django.urls import reverse_lazy
