96a3f4bccb06b301f58af3204aad438c908b1b01
petr <petr.cech@netbox.cz>
2023-12-14 19:25:28 +0100

    Update gunicorn command to use fixed values
    The gunicorn command in web-entrypoint.sh was previously using environment variables ${PORT} and ${WEB_CONCURRENCY}. Now, it has been updated to use fixed values (0.0.0.0:8000 for the bind address and 4 for workers) for simplicity and for testing purposes.

--- web-entrypoint.sh
+++ web-entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
 DJANGO_SUPERUSER_PASSWORD=12345 python manage.py createsuperuser --no-input --username=petr  --email=petr@cechpetr.cz
-gunicorn config.wsgi:application --bind 0.0.0.0:${PORT} --workers ${WEB_CONCURRENCY}
+gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 4
