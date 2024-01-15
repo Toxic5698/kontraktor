e60660fa411d98c207267965829d05b70fbdcfbd
petr <petr.cech@netbox.cz>
2024-01-15 10:45:13 +0100

    demo instance improvements

--- app/config/context_processors.py
+++ app/config/context_processors.py
@@ -2,5 +2,9 @@ import os
 
 
 def export_vars(request):
-    data = {'main_logo': os.getenv("MAIN_LOGO")}
+    data = {
+        'main_logo': os.getenv("MAIN_LOGO"),
+        'css_file': os.getenv("CSS_FILE"),
+        'environ': os.getenv("ENVIRONMENT"),
+    }
     return data
